7d955451b913ee183d711dea1a20631537459fb3
Sebastiaan Blaas <sebastiaan@avido.nl>
2023-03-10 11:08:56 +0100

    Add Bol Retailer API config file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Bol Retailer Api Configuration
+ */
+return [
+    'client_id' => env('BOL_CLIENT_ID'),
+    'client_secret' => env('BOL_CLIENT_SECRET'),
+    'test' => env('BOL_TEST_MODE')
+];
